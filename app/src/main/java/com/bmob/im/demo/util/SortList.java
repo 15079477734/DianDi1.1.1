96d608b048d28a7df967feaa8a5ab6e1c980bab5
15079477734 <462679107@qq.com>
2014-09-14 14:49:34 +0800

    1.1

--- app/src/main/java/com/bmob/im/demo/util/SortList.java
+++ app/src/main/java/com/bmob/im/demo/util/SortList.java
@@ -13,28 +13,42 @@ import java.util.List;
  * project:DianDi1.1.0 .
  * Copyright © sulinger .All Rights Reserved.
  */
-@SuppressWarnings("unchecked")
-public class SortList<E>{
-    public void Sort(List<E> list, final String method, final String sort){
-        Collections.sort(list, new Comparator() {
-            public int compare(Object a, Object b) {
-                int ret = 0;
-                try {
-                    Method m1 = ((E) a).getClass().getMethod(method, null);
-                    Method m2 = ((E) b).getClass().getMethod(method, null);
-                    if (sort != null && "desc".equals(sort))//倒序
-                        ret = m2.invoke(((E) b), null).toString().compareTo(m1.invoke(((E) a), null).toString());
-                    else//正序
-                        ret = m1.invoke(((E) a), null).toString().compareTo(m2.invoke(((E) b), null).toString());
-                } catch (NoSuchMethodException ne) {
-                    System.out.println(ne);
-                } catch (IllegalAccessException ie) {
-                    System.out.println(ie);
-                } catch (InvocationTargetException it) {
-                    System.out.println(it);
-                }
-                return ret;
+@SuppressWarnings("unckecked")
+public class SortList<E> {
+    public void Sort(List<E> list, final String method, final String sort) {
+        Collections.sort(list, new ListComparator(method, sort));
+    }
+
+    private class ListComparator implements Comparator<E> {
+
+        private String method;
+        private String sort;
+
+        public ListComparator(String method, String sort) {
+            this.method = method;
+            this.sort = sort;
+        }
+
+
+        public int compare(E a, E b) {
+            int ret = 0;
+
+            try {
+                Method m1 = ((E) a).getClass().getMethod(method, null);
+                Method m2 = ((E) b).getClass().getMethod(method, null);
+                if (sort != null && "desc".equals(sort))//倒序
+                    ret = m2.invoke(((E) b), null).toString().compareTo(m1.invoke(((E) a), null).toString());
+                else//正序
+                    ret = m1.invoke(((E) a), null).toString().compareTo(m2.invoke(((E) b), null).toString());
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
             }
-        });
+
+            return ret;
+        }
     }
 }
